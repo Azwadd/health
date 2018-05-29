1887db0053ef8b6032acae92c7acccfc7c96e6f2
Azwad <azwad.shameem0@gmail.com>
2018-05-29 21:53:25 +0000

    design

--- javascript/search.js
+++ javascript/search.js
@@ -1,22 +1,22 @@
 function firstResults(response) {
-    for (var i = 0; i < 100; i = i + 3) {
+    for (var i = 0; i < 39; i = i + 3) {
         console.log(response.results[i].thumbnail); 
-            $("#first").append("<div id="+i+"> <input class='image' type='image' src=" + response.results[i].thumbnail + ">" +
-            "<p class='text'>Has a total of " + response.results[i].nutrient_value + " Calories<p> </div>");
+        $("#first").append("<input id="+i+" class='image' type='image' data-calories="+response.results[i].nutrient_value+" src=" + response.results[i].thumbnail + ">" +
+        "<p class="+i+">Has a total of " + response.results[i].nutrient_value + " Calories<p> </div>");
     }
 }  
     
 function secondResults(response) {     
-    for (var i = 1; i < 100; i = i + 3) {
-        $("#second").append("<div id="+i+"> <input class='image' type='image' src=" + response.results[i].thumbnail + ">" +
-        "<p class='text'>Has a total of " + response.results[i].nutrient_value + " Calories<p> </div>");
+    for (var i = 1; i < 39; i = i + 3) {
+        $("#second").append("<input id="+i+" class='images' type='image' data-calories="+response.results[i].nutrient_value+" src=" + response.results[i].thumbnail + ">" +
+        "<p class="+i+">Has a total of " + response.results[i].nutrient_value + " Calories<p>");
     }
 }
 
 function thirdResults(response) {
-    for (var i = 2; i < 100; i = i + 3) {
-        $("#third").append("<div id="+i+"> <input class='images' type='image' src=" + response.results[i].thumbnail + ">" +
-        "<p class='text'>Has a total of " + response.results[i].nutrient_value + " Calories<p> </div>");
+    for (var i = 2; i < 39; i = i + 3) {
+        $("#third").append("<input id="+i+" class='images' type='image' data-calories="+response.results[i].nutrient_value+" src=" + response.results[i].thumbnail + ">" +
+        "<p class="+i+">Has a total of " + response.results[i].nutrient_value + " Calories<p>");
     }
 }
 
@@ -25,12 +25,16 @@ function linkToAPI(item) {
     return "https://apibeta.nutritionix.com/v2/search?q=" + item + "&fields=item_name,brand_name,item_id,nf_calories&appId=543a53ea&appKey=a2476d1824c2ff264a1a871eb1806f85";
 }
 
-function clearResponse(response){
+function clearResponse(response) { 
     $("#first").empty();
     $("#second").empty(); 
     $("#third").empty();
 }
 
+function clearCalories() {
+    $("#textcalories").empty();
+}
+var nutritionResponse;
 function output(item) {
     $.ajax({
         url: linkToAPI(item),
@@ -44,9 +48,32 @@ function output(item) {
         },
     });
 }
+
+function calorieCounter(item, i) {
+    $.ajax({
+        url: linkToAPI(item),
+        method: "GET",
+        success: function(response) {
+            
+        },
+    });
+}
+
+var calories = 0;
+function addCalories(itemCalories) {
+    console.log(calories, itemCalories);
+        calories = calories + itemCalories;
+        $("#textcalories").text(calories + "total calories");
+}
+
 $(document).ready(function() {
     $("#submit").click(function(e) {
         e.preventDefault();
         output($("#value").val());
     });
+    $(".results").on('click', 'input', function(event) {
+        clearCalories();
+        addCalories($(event.target).data('calories'));
+    });
+
 });
